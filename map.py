bc707b6d98bd378ece0a277da90bde098e311883
Bradley <breg2851@uni.sydney.edu.au>
2019-12-08 19:19:31 +1100

    Added changes to main.py, have not been able to test conversion as cairo is not working on my computer

--- map.py
+++ map.py
@@ -1,22 +1,27 @@
 # this is the main file for the colourful map generatior
 #Bradley Rego 2019
+#imports
 import os
+import random
+#getting dictonary and list from other files
 from visited_countries import visited_countries
 from countries import list_Countries
 
-
+#generates number between 1 and 255
 def randomColourGenerator():
     return [random.randint(1,255),random.randint(1,255),random.randint(1,255)]
 
 #reset the visited_countries
 def reset():
     list_Countries.clear()
+
 #converstions pdf and png
 #if cairo is not installed this will produce an error
 def conversion(scale,conversion_type):
     try:
         import cairo
     except ImportError as ex:
+        print(ex)
         print("Import Error, Please install cairo by typing: \n pip3 install cairo")
         return
     if conversion_type == "png":
@@ -36,19 +41,15 @@ def getMap():
     path =  os.path.join("WorldMap",filename)
     return path
 
+#returns countries 2 character code
 def getCode(country):
-    print(list_Countries.keys())
     return list_Countries.get(country)
 
-# def loop(selected, countries_visited):
-#     for i in countries_visited:
-#         if countries_visited == selected:
-#
-#
+# Main Function
 def main():
     while True:
-        country = input("enter the countries you have been to, enter 'finished' when you are done. \n")
-        if country == "finished":
+        country = input("enter the countries you have been to, enter 'done' when you are done. \n")
+        if country == "done":
             break
         else:
             # call country code finder
@@ -64,19 +65,23 @@ def main():
     #countries list
     edits = list()
     for country in list_Countries.values():
-        for i in countries_visited:
+        for i in visited_countries:
             if i  == country:
                 x  = randomColourGenerator()
                 colour = "rgb({},{},{})".format(x[0],x[1],x[2])
-                code.append("#{} {{fill: {};}}".format(country, colour))
-
-    map = world_map.replace("<!-- PYTHON Style1 -->", "\n".join(code))
+                y = "#{} {{fill: {};}}".format(country, colour)
+                edits.append(y)
+    #add edits to the world map
+    world_map = world_map.replace("<!-- EDITS GO HERE -->", "\n".join(edits))
 
 
     #write the changes:
     with open("output.svg", "w") as out_file:
         out_file.write(world_map)
+
+    # convert file
     conversion(1,"png")
+    
 
 if __name__ == "__main__":
     main()
